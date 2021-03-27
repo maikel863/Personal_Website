14a888f160facd960043e417a675414f8e879a44
Maikel <maikel863@outlook.com>
2021-03-27 17:46:39 -0400

    adjusted button style and positioning within CSS

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+// Create the button
 var button = document.createElement("button");
 button.innerHTML = "Click Me";
 
+// Append it somewhere
 var body = document.getElementsByTagName("body")[0];
 body.appendChild(button);
+
+// Add event handler/ create action for button
+button.addEventListener ("click", function() {
+    alert("Before FlatIron I knew absolutely nothing about JavaScript")
+});
